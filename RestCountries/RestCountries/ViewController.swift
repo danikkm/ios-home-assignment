a24628f3401e6e2193435e9772986c93220a4488
danikkm <dldanown@gmail.com>
2022-05-30 11:31:54 +0300

    Add starter RestCountries project.

---
+++ RestCountries/RestCountries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RestCountries
+//
+//  Created by Daniel Dluznevskij on 2022-05-30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
